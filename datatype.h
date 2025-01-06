0e9ddf74f342e1ef01caf4446924dd47f3bdd58a
phuonglinhle268 <plinhle26@gmail.com>
2025-01-06 13:57:56 +0700

    Add files via upload

---
+++ datatype.h
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+
+struct Student{
+	char  ID[10];
+	char numberOfCourse[10];
+	char Name[20];
+	char Email[20];
+	char Phone[10]; 
+};
+struct Course{
+	char studentID[10];
+	char Code[10];
+	char Name[20];
+}; 
+
+
+
+
